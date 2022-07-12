8802d7e6d1d9ea8c7bf7518cbbde0c137b015823
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-12 14:13:11 -0300

    primeiro commit

---
+++ PESSOA_MAIS_VELHA_POO/PESSOA.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PESSOA_MAIS_VELHA_POO
+{
+    internal class PESSOA
+    {
+        public string Nome;
+        public int Idade;
+
+    }
+}
---
+++ PESSOA_MAIS_VELHA_POO/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PESSOA_MAIS_VELHA_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PESSOA p1 = new PESSOA();
+            PESSOA p2 = new PESSOA();
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.Write("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            if (p1.Idade > p2.Idade)
+            {
+                Console.WriteLine($"Pessoa mais velha é {p1.Nome}");
+            }
+            else
+            {
+                Console.WriteLine($"Pessoa mais velha é {p2.Nome}");
+            }
+
+        }
+    }
+}
